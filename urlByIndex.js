18220d3332ea21fa89fa873ebc499aacb8bc73f2
cgusb <cgusbecker@gmail.com>
2023-11-15 16:13:57 -0500

    Add basic webring files

---
+++ urlByIndex.js
@@ -0,0 +1,7 @@
+const urlByIndex = [
+  "https://gusbus.space/DoodleBot.html",
+  "https://jazz-dude.com/Portfolio/doodles.html",
+  "https://art.bymegan.com/doodles.html",
+  "https://uuupah.neocities.org/art/my-art-2023/",
+  "https://yamasztuka.com/gallery.html"
+]
