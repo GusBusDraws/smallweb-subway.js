f5d8c968b236d3b6225815ea193184c56305ab5a
cgusb <cgusbecker@gmail.com>
2024-06-21 13:52:01 -0400

    Update variable, line color, and stations in poetry.js and poetry.json

--- poetry.js
+++ poetry.js
@@ -1,64 +1,64 @@
-let thisURL_creativesclub;
-let thisSite_creativesclub;
-let matchedSiteIndex_creativesclub;
-let matchedSite_creativesclub;
-let prevSiteIndex_creativesclub;
-let nextSiteIndex_creativesclub;
-const WEBRING_DATA_URL_creativesclub = `https://gusbus.space/smallweb-subway.js/creativesclub.json`;
-let DATA_creativesclub;
-loadWebringJSON_creativesclub(WEBRING_DATA_URL_creativesclub);
+let thisURL_poetry;
+let thisSite_poetry;
+let matchedSiteIndex_poetry;
+let matchedSite_poetry;
+let prevSiteIndex_poetry;
+let nextSiteIndex_poetry;
+const WEBRING_DATA_URL_poetry = `https://gusbus.space/smallweb-subway.js/poetry.json`;
+let DATA_poetry;
+loadWebringJSON_poetry(WEBRING_DATA_URL_poetry);
 
-function loadWebringJSON_creativesclub(url) {
+function loadWebringJSON_poetry(url) {
   fetch(url)
     .then(response => response.json())
-    .then((json) => {webringDataReady_creativesclub(json)});
+    .then((json) => {webringDataReady_poetry(json)});
 }
 
-function webringDataReady_creativesclub(json) {
-  DATA_creativesclub = json;
-  customElements.get('smallweb-subway-creativesclub') || (
-    customElements.define('smallweb-subway-creativesclub', Webring_creativesclub));
+function webringDataReady_poetry(json) {
+  DATA_poetry = json;
+  customElements.get('smallweb-subway-poetry') || (
+    customElements.define('smallweb-subway-poetry', Webring_poetry));
 }
 
-function getHostName_creativesclub(url) {
+function getHostName_poetry(url) {
   // this is a bit of a cheat that leverages the URL type to get the hostname automagically
   return new URL(url).hostname;
 }
 
-function goToPrev_creativesclub() {
+function goToPrev_poetry() {
   // Adding '//' treats the link as an external site, even without "https:"
-  location.href = '//' + DATA_creativesclub[prevSiteIndex_creativesclub].url
+  location.href = '//' + DATA_poetry[prevSiteIndex_poetry].url
 }
 
-function goToNext_creativesclub() {
+function goToNext_poetry() {
   // Adding '//' treats the link as an external site, even without "https:"
-  location.href = '//' + DATA_creativesclub[nextSiteIndex_creativesclub].url
+  location.href = '//' + DATA_poetry[nextSiteIndex_poetry].url
 }
 
-let template_creativesclub = document.createElement("template");
-template_creativesclub.innerHTML = `
-  <div class="webring_creativesclub">
+let template_poetry = document.createElement("template");
+template_poetry.innerHTML = `
+  <div class="webring_poetry">
     <h3>The Smallweb Subway</h3>
     <div>
-      <button id="tri-left_creativesclub" onclick="goToPrev_creativesclub()"></button>
+      <button id="tri-left_poetry" onclick="goToPrev_poetry()"></button>
       <div id="line">
         <div id="outer-circle">
           <div id="inner-circle"></div>
         </div>
       </div>
-      <button id="tri-right_creativesclub" onclick="goToNext_creativesclub()"></button>
+      <button id="tri-right_poetry" onclick="goToNext_poetry()"></button>
     </div>
     <p>
-      Creatives Club Line: Sites of Creatives Club members.
+      Poetry Line: Sites showcasing original poetry.
     </p>
   </div>
 
   <style>
-    .webring_creativesclub {
+    .webring_poetry {
       width: 100%;
       height: auto;
     }
-    .webring_creativesclub > div {
+    .webring_poetry > div {
       display: flex;
       gap: 20px;
       align-items: center;
@@ -75,34 +75,34 @@ template_creativesclub.innerHTML = `
       margin-bottom: 10px;
       padding-bottom: 10px;
     }
-    #tri-left_creativesclub {
+    #tri-left_poetry {
       width: 0;
       height: 0;
       background: none;
       border-top: 20px solid transparent;
       border-left: none;
-      border-right: 40px solid #fad447;
+      border-right: 40px solid #0077c0;
       border-bottom: 20px solid transparent;
       outline: none;
       cursor: pointer;
     }
-    #tri-left_creativesclub:hover {
+    #tri-left_poetry:hover {
       border-top: 20px solid transparent;
       border-left: none;
       border-right: 40px solid black;
       border-bottom: 20px solid transparent;
     }
-    #tri-right_creativesclub {
+    #tri-right_poetry {
       width: 0;
       height: 0;
       background: none;
       border-top: 20px solid transparent;
-      border-left: 40px solid #fad447;
+      border-left: 40px solid #0077c0;
       border-right: none;
       border-bottom: 20px solid transparent;
       cursor: pointer;
     }
-    #tri-right_creativesclub:hover {
+    #tri-right_poetry:hover {
       border-top: 20px solid transparent;
       border-left: 40px solid black;
       border-right: none;
@@ -111,7 +111,7 @@ template_creativesclub.innerHTML = `
     #line {
       width: 120px;
       height: 20px;
-      background: #fad447;
+      background: #0077c0;
       display: flex;
       align-items: center;
       justify-content: center;
@@ -134,38 +134,38 @@ template_creativesclub.innerHTML = `
   </style>
 `;
 
-class Webring_creativesclub extends HTMLElement {
+class Webring_poetry extends HTMLElement {
   constructor() {
     super()
     this.attachShadow({ mode: "open" })
-    this.shadowRoot.appendChild(template_creativesclub.content.cloneNode(true));
+    this.shadowRoot.appendChild(template_poetry.content.cloneNode(true));
   }
   connectedCallback() {
-    console.log('------------------')
-    console.log('creativesclub line')
-    console.log('------------------')
+    console.log('-----------')
+    console.log('poetry line')
+    console.log('-----------')
     console.log('Webring JSON data:')
-    console.log(JSON.stringify(DATA_creativesclub))
-    thisURL_creativesclub = new URL(window.location.href);
-    thisSite_creativesclub = (
-      thisURL_creativesclub.hostname + thisURL_creativesclub.pathname)
+    console.log(JSON.stringify(DATA_poetry))
+    thisURL_poetry = new URL(window.location.href);
+    thisSite_poetry = (
+      thisURL_poetry.hostname + thisURL_poetry.pathname)
     console.log("This site:")
-    console.log(thisSite_creativesclub)
-    matchedSiteIndex_creativesclub = (
-      DATA_creativesclub.map(x => x.url).indexOf(thisSite_creativesclub))
-    matchedSite_creativesclub = (
-      DATA_creativesclub[matchedSiteIndex_creativesclub]);
+    console.log(thisSite_poetry)
+    matchedSiteIndex_poetry = (
+      DATA_poetry.map(x => x.url).indexOf(thisSite_poetry))
+    matchedSite_poetry = (
+      DATA_poetry[matchedSiteIndex_poetry]);
     console.log("Matched site:")
-    console.log(matchedSite_creativesclub.url)
-    prevSiteIndex_creativesclub = matchedSiteIndex_creativesclub - 1;
-    if (prevSiteIndex_creativesclub === -1) {
-      prevSiteIndex_creativesclub = DATA_creativesclub.length - 1};
+    console.log(matchedSite_poetry.url)
+    prevSiteIndex_poetry = matchedSiteIndex_poetry - 1;
+    if (prevSiteIndex_poetry === -1) {
+      prevSiteIndex_poetry = DATA_poetry.length - 1};
     console.log("Previous site:")
-    console.log(DATA_creativesclub[prevSiteIndex_creativesclub].url)
-    nextSiteIndex_creativesclub = matchedSiteIndex_creativesclub + 1;
-    if (nextSiteIndex_creativesclub === DATA_creativesclub.length) {
-      nextSiteIndex_creativesclub = 0};
+    console.log(DATA_poetry[prevSiteIndex_poetry].url)
+    nextSiteIndex_poetry = matchedSiteIndex_poetry + 1;
+    if (nextSiteIndex_poetry === DATA_poetry.length) {
+      nextSiteIndex_poetry = 0};
     console.log("Next site:")
-    console.log(DATA_creativesclub[nextSiteIndex_creativesclub].url)
+    console.log(DATA_poetry[nextSiteIndex_poetry].url)
   }
 }
